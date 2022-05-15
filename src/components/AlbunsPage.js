eab308fc03274e3d61f6549676c3e8dc5d011b41
Hermes Júnior <hermesj.junior@gmail.com>
2022-05-15 18:24:50 -0300

    Ajustes AlbunsPage

--- src/components/AlbunsPage.js
+++ src/components/AlbunsPage.js
@@ -5,6 +5,8 @@ import { useNavigate } from 'react-router-dom';
 
 import UserContext from '../context/UserContext';
 import Header from "./Header";
+import Main from "./stylesAll/Main";
+import CapaAlbum from './stylesAll/CapaAlbum';
 
 function AlbunsPage() {
 
@@ -29,12 +31,14 @@ function AlbunsPage() {
         <BodyAlbum>
             {
             filterCategories.map(vinil => {
-                const {banda, album, url, preco } = vinil;
+                const { _id, banda, album, url, preco } = vinil;
                 return  (
-                   <CapaAlbum imagePath={posterURL}>
-                        <Link to={`/carrinho/${id}`}>
-                            <img src={posterURL} alt={title} key={id}/>
+                   <CapaAlbum imagePath={url}>
+                        <Link to={`/carrinho/${_id}`}>
+                            <img src={url} alt={title} key={_id}/>
+                            <p>{`${banda} - ${album}`}</p>
                         </Link>
+                        <p>{`por: R$ ${preco}`}</p>
                   </CapaAlbum>
                    )
                 })
@@ -47,5 +51,13 @@ function AlbunsPage() {
         </Main>
     );
 }
+
+const BodyAlbum = styled.section`
+    width: 100vw;
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: center;
+    `
 
 export default AlbunsPage
---
+++ src/components/stylesAll/CapaAlbum.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const CapaAlbum = styled.article`
+    width: 145px;
+    height: 210px;
+    background: #FFFFFF;
+    box-shadow: 0px 2px 4px 2px rgba(0, 0, 0, 0.1);
+    border-radius: 3px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 15px;
+    
+    img{
+        width: 130px;
+        height: 195px;
+    }
+`
+
+export default CapaAlbum;
